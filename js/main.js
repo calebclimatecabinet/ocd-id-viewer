95c45f349ffb21c683c3b7cc6d5aff17f970d94c
James McKinney <james@slashpoundbang.com>
2014-02-07 17:07:07 -0500

    try to get recline working

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
+// http://jsonpdataproxy.appspot.com/?max-results=10000&url=https://raw.github.com/opencivicdata/ocd-division-ids/master/identifiers/country-ca.csv
 var dataset = new recline.Model.Dataset({
-  backend: 'csv',
-  url: 'http://raw.github.com/opencivicdata/ocd-division-ids/master/identifiers/country-ca.csv'
+  backend: 'dataproxy',
+  rows: 10000,
+  url: 'https://raw.github.com/opencivicdata/ocd-division-ids/master/identifiers/country-ca.csv'
+});
+
+dataset.fetch().done(function (dataset) {
+  console.log(dataset.records.length);
+  var grid = new recline.View.SlickGrid({
+    model: dataset,
+    el: $('#grid')
+  });
+  // $('#grid').append(grid.el);
 });
