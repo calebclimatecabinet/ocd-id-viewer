8591e7a67c524d1a7d512937cf853f81b336178d
James McKinney <james@slashpoundbang.com>
2014-02-07 18:46:42 -0500

    add typeahead

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
-// http://jsonpdataproxy.appspot.com/?max-results=10000&url=https://raw.github.com/opencivicdata/ocd-division-ids/master/identifiers/country-ca.csv
-var dataset = new recline.Model.Dataset({
-  backend: 'dataproxy',
-  rows: 10000,
-  url: 'https://raw.github.com/opencivicdata/ocd-division-ids/master/identifiers/country-ca.csv'
+// @see https://github.com/twitter/typeahead.js/blob/master/doc/bloodhound.md
+var engine = new Bloodhound({
+  datumTokenizer: function (datum) {
+    return Bloodhound.tokenizers.whitespace(datum.name);
+  },
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  prefetch: {
+    url: 'http://jsonpdataproxy.appspot.com/?format=json&max-results=10000&url=https://raw.github.com/opencivicdata/ocd-division-ids/master/identifiers/country-ca.csv',
+    filter: function (data) {
+      return $.map([data.fields].concat(data.data), function (row) {
+        return {identifier: row[0], name: row[1]};
+      });
+    }
+  }
 });
 
-dataset.fetch().done(function (dataset) {
-  console.log(dataset.records.length);
-  var grid = new recline.View.SlickGrid({
-    model: dataset,
-    el: $('#grid')
+engine.initialize().done(function () {
+  $('#typeahead').typeahead({
+    autoselect: true,
+    highlight: true
+  }, {
+    displayKey: 'name',
+    source: engine.ttAdapter(),
+    templates: {
+      suggestion: Handlebars.compile('<p><span class="name">{{name}}</span><span class="identifier">{{identifier}}</span></p>')
+    }
   });
-  // $('#grid').append(grid.el);
+
+  $('#typeahead').bind('typeahead:selected', function (event, datum, name) {
+  });
 });
